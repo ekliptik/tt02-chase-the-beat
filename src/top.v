e013883a58fdf76ef64b63ad33617eee2fe27b1c
Emil Jiří Tywoniak <emil.tywoniak@gmail.com>
2022-11-10 00:38:27 +0100

    fix build attempt 3

--- src/top.v
+++ src/top.v
@@ -2,5 +2,5 @@ module top (
   input [7:0] io_in,
   output [7:0] io_out
 );
-    chase chase(.io_out(io_out), .clk(io_in[0]), rst(io_in[1]), tap(io_in[0]));
+    chase chase(.io_out(io_out), .clk(io_in[0]), .rst(io_in[1]), .tap(io_in[0]));
 endmodule
